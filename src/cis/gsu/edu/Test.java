cfcbbf5ceebb25539cc4c313dc9955e44cdde33d
markfawaz <mark.fawaz@gmail.com>
2016-09-21 22:39:52 -0400

    nice loo

--- src/cis/gsu/edu/Test.java
+++ src/cis/gsu/edu/Test.java
@@ -3,7 +3,8 @@ package cis.gsu.edu;
 public class Test {
 	public static void main(String[] args) {
 		
-		System.out.print("This is a test.");
+		System.out.println("This is a test.");
+		System.out.println("Thanks for the clear instructions!");
 			
 	}
 
