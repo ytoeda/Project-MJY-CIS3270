394a790157cc5b9c6df898e86898e73a49d93d49
ytoeda <ytoeda@gmail.com>
2016-11-29 20:15:28 -0500

    Created test class.

--- src/cis/gsu/edu/Test.java
+++ src/cis/gsu/edu/Test.java
@@ -1,11 +1,10 @@
 package cis.gsu.edu;
 
 public class Test {
+
 	public static void main(String[] args) {
-		
-		System.out.println("This is a test.");
-		System.out.println("Thanks for the clear instructions!");
-		System.out.println();	
+		// TODO Auto-generated method stub
+
 	}
 
 }
