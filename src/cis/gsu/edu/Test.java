cf4e658aabb4ba182dfa9c36e7adfbdc4a4cfb97
ytoeda <ytoeda@gmail.com>
2016-12-04 21:11:06 -0500

    hhh

--- src/cis/gsu/edu/Test.java
+++ src/cis/gsu/edu/Test.java
@@ -1,10 +1,25 @@
 package cis.gsu.edu;
 
-public class Test {
+import javax.swing.*;
+import java.awt.*;
 
+public class Test extends JFrame {
+	
+	ImageIcon myImage = new ImageIcon("image/SplashImage.gif");
+	
+	public Test(){
+		setLayout(new GridLayout(1,4,5,5));
+		add(new JLabel(myImage));
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Test frame = new Test();
+		frame.setTitle("TestImageIcon");
+		frame.setSize(200,200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
 	}
-
 }
+
