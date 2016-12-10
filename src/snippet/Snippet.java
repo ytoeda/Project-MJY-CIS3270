41a8765acb764dc2771919e6ebf6fcd2ef78f31a
markfawaz <mark.fawaz@gmail.com>
2016-12-10 17:20:29 -0500

    Commit

--- src/snippet/Snippet.java
+++
@@ -1,6 +0,0 @@
-package snippet;
-
-public class Snippet {
-	"Make Coverage More Secure for Those Who Have Insurance, and Extend Affordable Coverage to the Uninsured." Make Coverage More Secure for Those Who Have Insurance, and Extend Affordable Coverage to the Uninsured | Performance.gov. N.p., 02 Apr. 2014. Web. 02 Dec. 2016.
-}
-
